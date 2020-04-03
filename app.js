32148389f99821e38a7e6cd9afdc3a1c2ddaadfb
DMesure <damien.mesure@student.ehb.be>
2020-04-03 23:37:02 +0200

    small tweaks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from Node.JS!'));
+app.get('/', (req, res) => res.send('Hello from Node.JS, this is going to be the REST API for our mailinglist!'));
 
 app.listen(3000, () => {
     console.log('My REST API for MailingList running on port 3000!');
